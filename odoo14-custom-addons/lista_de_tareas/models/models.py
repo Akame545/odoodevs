21d00521b22db172c662eca76e8fadb2f202b62a
Alejandro Martin <alejandro.mf1002@gmail.com>
2022-02-18 08:32:13 +0100

    starting pro task list

--- odoo14-custom-addons/lista_de_tareas/models/models.py
+++ odoo14-custom-addons/lista_de_tareas/models/models.py
@@ -1,25 +1,20 @@
 # -*- coding: utf-8 -*-
-from pickle import TRUE
 from odoo import models, fields, api
 class lista_de_tareas(models.Model):
     _name = 'lista_de_tareas.lista_de_tareas'
     _description = 'lista_de_tareas.lista_de_tareas'
-#   Elementos de cada fila del modelo de datos
-#   Los tipos de datos a usar en el ORM son
-#   https://www.odoo-com/documentation/14.0/developer/reference/addons/orm.html
-#   fields
+
+    avatar = fields.Image('Imagen tareas',max_width=50,max_heigth=50)
     tarea = fields.Char()
     prioridad = fields.Integer()
     urgente = fields.Boolean(compute="_value_urgente", store=True)
-    fechaFin = fields.Date()
+    fecha = fields.Date()
     realizada = fields.Boolean()
 
     #Este computo depende de la variable prioridad
     @api.depends('prioridad')
     #Funcion para calcular el valor de urgente
     def _value_urgente(self):
-    
-    
     #Para cada registro
         for record in self:
         #Si la prioridad es mayor que 10, se considera urgente, en otro caso no lo es
