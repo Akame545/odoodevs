38dbf0e34dffbaa480c17d9f0ef7305a9b81391c
Alejandro Martin <alejandro.mf1002@gmail.com>
2022-03-01 09:51:08 +0100

    Final version

--- odoo14-custom-addons/milkway/models/models.py
+++ odoo14-custom-addons/milkway/models/models.py
@@ -4,15 +4,29 @@ from odoo import models, fields, api
 
 
 class milkway(models.Model):
-     _name = 'milkway.milkway'
-     _description = 'milkway.milkway'
+    _name = 'milkway.milkway'
+    _description = 'milkway.milkway'
+    _rec_name = 'display_name'
 
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+    color = fields.Integer()
+    display_name = fields.Char(string='Name', required=True)
+    email = fields.Char('Email', required=True)
+    mobile = fields.Char('Mobile', required=True)
+    # avatar = fields.Image('Imagen tareas',max_width=50,max_heigth=50)
+    # tarea = fields.Char()
+    # prioridad = fields.Integer()
+    # urgente = fields.Boolean(compute="_value_urgente", store=True)
+    # fecha = fields.Date()
+    # realizada = fields.Boolean()
 
-     @api.depends('value')
-     def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+    # #Este computo depende de la variable prioridad
+    # @api.depends('prioridad')
+    # #Funcion para calcular el valor de urgente
+    # def _value_urgente(self):
+    # #Para cada registro
+    #     for record in self:
+    #     #Si la prioridad es mayor que 10, se considera urgente, en otro caso no lo es
+    #         if record.prioridad>10:
+    #             record.urgente = True
+    #         else:
+    #             record.urgente = False
